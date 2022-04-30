b524c0912c1bec0bf65a18b732da3e41c194f63d
jsanchezl12 <js.sanchezl12@uniandes.edu.co>
2022-04-30 07:01:07 -0400

    Punto 2: Listado de tipos de cafes junto con su cantidad

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -10,18 +10,58 @@ import { CafeService } from '../cafe.service';
 export class CafeListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
-
+  totalOrigen: number = 0;
+  totalBlend: number = 0;
+  tiposCafes: Array<string> = [];
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
     this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      for(let cafe of this.cafes){
+        if(!this.tiposCafes.includes(cafe.tipo)){
+          this.tiposCafes.push(cafe.tipo);
+        }
+      }
     });
+  }
+
+  
+  /*
+  getTotalOrigen(): number {
+    this.totalOrigen = 0;
+    for (let cafe of this.cafes) {
+      if (cafe.tipo == "Café de Origen") {
+        this.totalOrigen += 1;
+      }
+    }
+    return this.totalOrigen;
+  }
+
+  getTotalBlend(): number {
+    this.totalBlend = 0;
+    for (let cafe of this.cafes) {
+      if (cafe.tipo == "Blend") {
+        this.totalBlend += 1;
+      }
+    }
+    return this.totalBlend;
+  }*/
+
+  getTotalByTipo(tipo: string): number {
+    let total: number = 0;
+    for (let cafe of this.cafes) {
+      if (cafe.tipo == tipo) {
+        total += 1;
+      }
+    }
+    return total;
   }
 
   ngOnInit() {
     this.getCafes();
+    
   }
 
 }
