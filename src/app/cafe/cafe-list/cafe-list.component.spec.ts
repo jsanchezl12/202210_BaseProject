24ed15320fec0109c1f8fce9da498347fcf959d2
jsanchezl12 <js.sanchezl12@uniandes.edu.co>
2022-04-30 20:20:46 -0400

    Punto 3 - Pruebas de 3 Filas Mas la fila del encabezado

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,18 +2,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { CafeListComponent } from './cafe-list.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
+
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [ CafeListComponent ]
+      declarations: [ CafeListComponent ],
+      providers: [CafeService]
     })
     .compileComponents();
   }));
@@ -21,10 +27,45 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    component.cafes = [];
+    //CAFE 1
+    component.cafes.push(new Cafe(faker.datatype.number(),
+    faker.commerce.productName(),
+    faker.commerce.productMaterial(),
+    faker.commerce.department(),
+    faker.lorem.sentence(),
+    faker.datatype.number(),
+    faker.image.imageUrl()));
+    //CAFE 2
+    component.cafes.push(new Cafe(faker.datatype.number(),
+    faker.commerce.productName(),
+    faker.commerce.productMaterial(),
+    faker.commerce.department(),
+    faker.lorem.sentence(),
+    faker.datatype.number(),
+    faker.image.imageUrl()));
+    //CAFE 3
+    component.cafes.push(new Cafe(faker.datatype.number(),
+    faker.commerce.productName(),
+    faker.commerce.productMaterial(),
+    faker.commerce.department(),
+    faker.lorem.sentence(),
+    faker.datatype.number(),
+    faker.image.imageUrl()));
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show 3 cafes', () => {
+    expect(component.cafes.length).toBe(3);
+    const rows = fixture.debugElement.queryAll(By.css('tr'));
+    //3 CAFE + Header
+    expect(rows.length).toBe(4);
+  });
+
 });
